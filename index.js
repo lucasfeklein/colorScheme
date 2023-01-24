6f2b0e771c8bef453d8356cb777b14fd6149c49e
Klein <116522160+lucasfeklein@users.noreply.github.com>
2023-01-23 21:59:29 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 
 
-document.getElementById('btn').addEventListener('click', render)
 
-function render(){
-const mode = document.getElementById('select-mode').value.toLowerCase()
-const colorHex = document.getElementById('select-color').value.substring(1)
 
-fetch (`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${mode}&count=5`)
-    .then (response => response.json())
-    .then (data => {
+function render() {
+    const mode = document.getElementById('select-mode').value.toLowerCase()
+    const colorHex = document.getElementById('select-color').value.substring(1)
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${mode}&count=5`)
+        .then(response => response.json())
+        .then(data => {
             let colorHtml = ''
             let hexHtml = ''
             const colorsArray = data.colors
-            for(let color of colorsArray){
+            let teste = false
+            for (let color of colorsArray) {
                 colorHtml += `
                     <div>
-                        <div style='background: ${color.hex.value}' id="palette-color"></div>     
+                        <div style='background: ${color.hex.value}' class="palette-color" data-color=${color.hex.value}>
+                            <span class='copy' id=${color.hex.value}></span>
+                        </div>     
                         <span>${color.hex.value}</span>
                     </div>
+                    
                 `
             }
             document.getElementById('color-container').innerHTML = colorHtml
+
+            document.querySelectorAll('[data-color]').forEach(el => {
+                el.addEventListener('click', () => {
+                    navigator.clipboard.writeText(el.dataset.color)
+                    document.getElementById(el.dataset.color).innerText = 'Copied!'
+                    document.getElementById(el.dataset.color).style.display = 'flex';
+                    setTimeout(function () {
+                        document.getElementById(el.dataset.color).style.display = 'none';
+                    }, 600)
+                })
+
+                el.addEventListener('mouseover', () => {
+                    document.getElementById(el.dataset.color).innerText = 'Copy!'
+                    document.getElementById(el.dataset.color).style.display = 'flex';
+                })
+
+                el.addEventListener('mouseout', () => {
+                    document.getElementById(el.dataset.color).innerText = ''
+                    document.getElementById(el.dataset.color).style.display = 'none';
+                })
+            }
+            )
+
+
+
         })
 }
 
+
+
+
+document.getElementById('btn').addEventListener('click', render)
 render()
